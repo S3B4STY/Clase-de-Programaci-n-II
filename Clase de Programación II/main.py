52f7f6db3df903ec3a3036791beac54a73373125
S3B4STY <sebastian00llanos@gmail.com>
2026-07-02 17:48:47 -0500

    Update main.py

--- Clase de Programación II/main.py
+++ Clase de Programación II/main.py
@@ -1,22 +1,13 @@
 from views.menu import menu_principal
 
-def mostrar_bienvenida():
-
-    print("=" * 50)
-    print("      SISTEMA DE FACTURACIÓN")
-    print("=" * 50)
-    print("Proyecto desarrollado en Python")
-    print("Base de datos: Excel")
-    print("=" * 50)
-
 
 def main():
 
-    mostrar_bienvenida()
+    print("=" * 50)
+    print(" SISTEMA DE FACTURACIÓN ")
+    print("=" * 50)
 
     menu_principal()
-
-    print("\nPrograma finalizado correctamente.")
 
 
 if __name__ == "__main__":
