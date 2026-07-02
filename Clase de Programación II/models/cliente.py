d615d5dedf9120a597f1780a14ce087a8af3c9e9
S3B4STY <sebastian00llanos@gmail.com>
2026-07-02 17:50:00 -0500

    Refactor Cliente class with fewer attributes
    Refactor Cliente class to simplify attributes and constructor.

--- Clase de Programación II/models/cliente.py
+++ Clase de Programación II/models/cliente.py
@@ -1,12 +1,7 @@
 class Cliente:
-    def __init__(self, id, cedula, nombres, apellidos, telefono, correo, direccion):
-        self.id = id
-        self.cedula = cedula
-        self.nombres = nombres
-        self.apellidos = apellidos
-        self.telefono = telefono
-        self.correo = correo
-        self.direccion = direccion
 
-    def to_dict(self):
-        return f"{self.nombres} {self.apellidos} - {self.cedula}"
+    def __init__(self, nombre, cedula, telefono):
+
+        self.nombre = nombre
+        self.cedula = cedula
+        self.telefono = telefono
