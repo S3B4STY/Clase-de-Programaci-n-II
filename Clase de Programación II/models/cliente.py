8cc9409c643022913b674e663bf6fcb5354a5a13
jonamayru07-collab <jonamayru07@gmail.com>
2026-07-02 17:29:09 -0500

    Update cliente.py

--- Clase de Programación II/models/cliente.py
+++ Clase de Programación II/models/cliente.py
@@ -1,10 +1,12 @@
 class Cliente:
-    def __init__(self, id, nombre):
+    def __init__(self, id, cedula, nombres, apellidos, telefono, correo, direccion):
         self.id = id
-        self.nombre = nombre
+        self.cedula = cedula
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.telefono = telefono
+        self.correo = correo
+        self.direccion = direccion
 
     def to_dict(self):
-        return {
-            "id": self.id,
-            "nombre": self.nombre
-        }
+        return f"{self.nombres} {self.apellidos} - {self.cedula}"
