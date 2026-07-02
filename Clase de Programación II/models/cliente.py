a3d8bc7790ec8e735e189aa32f469271d68553a2
anthonyparreo98-cloud <anthony.parreo98@gmail.com>
2026-07-02 17:21:25 -0500

    Add Cliente class

---
+++ Clase de Programación II/models/cliente.py
@@ -0,0 +1,10 @@
+class Cliente:
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre
+        }
